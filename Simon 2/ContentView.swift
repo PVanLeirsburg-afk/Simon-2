7ada4231bfbc688575f27cd76f4db5118564723b
Pam VanLeirsburg <pvanleirsburg@barrington220.org>
2026-08-03 18:04:45 -0500

    Initial Commit

---
+++ Simon 2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Simon 2
+//
+//  Created by Pamela VanLeirsburg on 8/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Simon 2/Simon_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Simon_2App.swift
+//  Simon 2
+//
+//  Created by Pamela VanLeirsburg on 8/3/26.
+//
+
+import SwiftUI
+
+@main
+struct Simon_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
